b01f6fddfea09476aee7c987d32e009045e1c9ce
Joseph Blodgett <Joseph.Blodgett@gmail.com>
2016-01-14 17:00:38 -0700

    Changes to run on Port 80

--- webServer/server.go
+++ webServer/server.go
@@ -2,41 +2,41 @@ package main
 
 import (
 	"fmt"
-  "net/http"
-  "io/ioutil"
+	"io/ioutil"
+	"net/http"
 )
 
 func loadIndexPage(w http.ResponseWriter, r *http.Request) {
-  if r.URL.Path[1:] == "" {
-    fmt.Fprintf(w, getStaticPage("index.html"))
-  } else {
-    fmt.Fprintf(w, getStaticPage(r.URL.Path[1:]))
-  }
+	if r.URL.Path[1:] == "" {
+		fmt.Fprintf(w, getStaticPage("index.html"))
+	} else {
+		fmt.Fprintf(w, getStaticPage(r.URL.Path[1:]))
+	}
 
 }
 
 func loadImages(w http.ResponseWriter, r *http.Request) {
-  fmt.Println("images " + r.URL.Path[1:])
+	fmt.Println("images " + r.URL.Path[1:])
 }
 
 func getStaticPage(path string) string {
-  path = "Static/" + path
+	path = "Static/" + path
 
-  dat, err := ioutil.ReadFile(path)
+	dat, err := ioutil.ReadFile(path)
 
-  check(err)
+	check(err)
 
-  return string(dat)
+	return string(dat)
 }
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func main() {
-    http.HandleFunc("/", loadIndexPage)
-    http.HandleFunc("/images/", loadImages)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", loadIndexPage)
+	http.HandleFunc("/images/", loadImages)
+	http.ListenAndServe(":80", nil)
 }
